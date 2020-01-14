108d986fdc176eb7cc403a2a05d753ccfef26b6b
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-01-14 11:21:35 +0100

    Sets and Maps

---
+++ assets/scripts/setsmaps.js
@@ -0,0 +1,32 @@
+/* const ids = new Set(['Hi', 'Lion']);
+console.log(ids.add('Miguel'));
+
+ids.delete('Hi')
+
+for(const entries of ids.entries()) {
+    console.log(entries[1])
+} */
+
+const person1 = {name: 'Lionel'};
+const person2 = {name: 'Miguel'};
+
+const personData = new Map([[person1, [{date: 'Monday', time: '4:00 PM'}]]]);
+
+personData.set(person2, [{date: 'Other', time: '10:00 AM'}])
+
+
+console.log(personData);
+
+console.log(personData.get(person1))
+
+for (const entry of personData.entries()) {
+    console.log(entry)
+}
+
+for (const [key, value] of personData.entries()) {
+    console.log(key, value)
+}
+
+for (const key of personData.keys()) {
+    console.log(key)
+}
